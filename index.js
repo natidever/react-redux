71f50d0e2e99bbbb4037f2b5550e6a4118b214d1
natidever <natnael7820@gmail.com>
2024-01-25 19:46:47 +0300

    Redux store

--- index.js
+++ index.js
@@ -1,40 +1,74 @@
 const BUY_CAKE ="BUY_CAKE"
+const BUY_ICECREAM= "BUY_ICECREAM"
 const redux = require('redux')
 const createStore = redux.legacy_createStore
+const combineReducers = redux.combineReducers
 
 //STORE
 //ACTION 
 //REDUCER
-
 //=======================================================ACTION========================================
 function buy_cake(){
     return {
         type:BUY_CAKE,
-        info:"First redux action"
+     
     }
 }
-//=======================================================ACTION========================================
+
+function buy_icecream(){
+   return{
+    type:BUY_ICECREAM
+   }
+}
+
 
 
 //reducer is function that tell what how the state is changed it use two informatino to do that Action (to know what to do ) the state to chage it 
 //State
-const initialState = {
+const initialCake = {
     numberOfCake:20
+ }
+ const initialIceCream = {
+    numberOfIceCream:10
  }
 
 //=======================================================Reducer========================================
  
-const reducer =(state=initialState,action)=>{
+const cakeReducer =(state=initialCake,action)=>{
     switch (action.type){
         case BUY_CAKE:return{
             ...state,
             numberOfCake:state.numberOfCake-1
         }
+        default :return state
+        
     }
 }
-const store=createStore(reducer)
+const iceCreamReducer =(state = initialIceCream,action )=>{
+    switch(action.type){
+
+        case BUY_ICECREAM :return{
+        ...state,
+
+            numberOfIceCream:state.numberOfIceCream -1
+        }
+        default :return state
+    }
+}
+
+
+
+const rootReducer=combineReducers({
+    cake:cakeReducer,
+    iceCream:iceCreamReducer,
+})
+
+
+const store=createStore(rootReducer)
 const unsubscribe = store.subscribe(()=>console.log('updated stae',store.getState()))
+
 store.dispatch(buy_cake())
 store.dispatch(buy_cake())
-store.dispatch(buy_cake())
+store.dispatch(buy_icecream())
+
 unsubscribe()
